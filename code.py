a3b1b8427c048c01d3179d01caabd424cc86b3a2
Gideon Suttle <gideonsuttle777@gmail.com>
2023-08-12 17:35:44 +0530

    yay!

--- code.py
+++ code.py
@@ -7,7 +7,7 @@ import streamlit as st
 from PIL import Image
 import streamlit as st
 st.header("AI story image generator")
-llm = Cohere(cohere_api_key="Q6ryuEOendZO0gOYBrToKNctroY7ehJOkkxeueV2")
+llm = Cohere(cohere_api_key="YOUR API KEY (sorry nidhin)")
 
 
 def generate_image(prompt):
@@ -15,7 +15,7 @@ def generate_image(prompt):
         files={
             'prompt': (None, prompt, 'response')
         },
-        headers={'x-api-key': '1051f780910c468e9b562f10427467ed86261f932d85a9d8b73062d7b30d27a86654934a75079f7890cf02712bf67337'}
+        headers={'x-api-key': 'YOUR API KEY (sorry nidhin)'}
     )
 
     if r.ok:
